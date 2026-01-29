b19239a775778c972ff1ee91d79293fb8f03655d
ExceptedPrism3 <ben.ayoube@outlook.com>
2026-01-29 20:43:14 +0100

    Fix: Remove Mod 02 strict docstrings (v1.5.5)

--- germinette/subjects/python_module_02.py
+++ germinette/subjects/python_module_02.py
@@ -112,12 +112,7 @@ class Tester(BaseTester):
                  self.record_error(exercise_label, "Style Error (Flake8)", style_errors)
                  return None, None
             
-            # Check Docstrings (Module 02 Strictness)
-            doc_errors = self.check_docstrings(found_path)
-            if doc_errors:
-                 console.print("[red]KO[/red]")
-                 self.record_error(exercise_label, "Style Error (Missing Docstrings)", doc_errors)
-                 return None, None
+
             
             # Check Type Hints (Mandatory in module 2)
             type_hint_errors = self.check_type_hints(found_path)
